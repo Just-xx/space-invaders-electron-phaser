ed7c7bdbd914a3a31964bcb98b0dc0ebf7f676f2
Jakub <kubadreczz@gmail.com>
2025-12-03 19:35:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = !app.isPackaged;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: false,
+    resizable: true
+  });
+
+  win.maximize();
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile('src/index.html');
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
